947e5c617ede0cf41ced2bc171444bcf0447fa5c
Nimish <shailbandha@gmail.com>
2022-01-04 19:48:07 +0530

    project section
    added live demo btn

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,13 +7,13 @@ function Projects() {
             <h1>Projects</h1>
             <section className="projects-section">
                 <Project host="codepen" title='Calculator' photoURL="/Images/calculator.png" source="https://codepen.io/Nimish_A380/pen/MWmYgjO" />
-                <Project host="github" title='Crypto Price Tracker' photoURL="/Images/cryptopricelist.png" source="https://github.com/Nimish22211/CryptoPrice" />
-                <Project host="github" title='TodoList' photoURL="/Images/todolist.png" source="https://github.com/Nimish22211/Todolist" />
-                <Project host="github" title='Medidata' photoURL="/Images/medidata.png" source="https://github.com/Nimish22211/MediData" />
+                <Project live="https://cryptopricelist.netlify.app" host="github" title='Crypto Price Tracker' photoURL="/Images/cryptopricelist.png" source="https://github.com/Nimish22211/CryptoPrice" />
+                <Project live="https://yourtodolist.netlify.app" host="github" title='TodoList' photoURL="/Images/todolist.png" source="https://github.com/Nimish22211/Todolist" />
+                <Project live="http://medidata.netlify.app" host="github" title='Medidata' photoURL="/Images/medidata.png" source="https://github.com/Nimish22211/MediData" />
                 <Project host="codepen" title='Pomodoro clock' photoURL="/Images/pomodoro-clock.png" source="https://codepen.io/Nimish_A380/pen/qBmYwrX" />
-                <Project host="github" title='Lesson Tracker' photoURL="/Images/lesson-tracker.png" source="https://github.com/Nimish22211/LessonTrackerApp" />
-                <Project host="github" title='Rubiks Timer' photoURL="/Images/rubiks timer.png" source="https://github.com/Nimish22211/Rubiks-Timer" />
-                <Project host="github" title='JS Quiz' photoURL="/Images/js quiz.png" source="https://github.com/Nimish22211/Quiz-with-React" />
+                <Project live="https://lessontracker.netlify.app" host="github" title='Lesson Tracker' photoURL="/Images/lesson-tracker.png" source="https://github.com/Nimish22211/LessonTrackerApp" />
+                <Project live="https://yourrubikstimer.netlify.app" host="github" title='Rubiks Timer' photoURL="/Images/rubiks timer.png" source="https://github.com/Nimish22211/Rubiks-Timer" />
+                <Project live="https://javascriptquiz.netlify.app" host="github" title='JS Quiz' photoURL="/Images/js quiz.png" source="https://github.com/Nimish22211/Quiz-with-React" />
             </section>
         </div>
     )
